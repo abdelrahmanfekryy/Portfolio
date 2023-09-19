0b826a9fc01fb11f5e2879f87cae9edc175bb63d
abdelrahmanfekryy <https://github.com/abdelrahmanfekryy>
2023-09-19 18:03:39 +0300

    edit

---
+++ assets/js/certificates.js
@@ -0,0 +1,317 @@
+const certificates_section = document.querySelector(".certificates");
+
+certificates_section.innerHTML =
+`
+<header>
+<h2 class="h2 article-title">Certificates</h2>
+</header>
+
+<section class="projects">
+
+  <ul class="filter-list" data-filter-list>
+
+    <li class="filter-item">
+
+      <button class="active" data-filter-btn-video>
+        All
+      </button>
+
+    </li>
+
+  </ul>
+
+  <div class="filter-select-box">
+
+    <button class="filter-select" data-select-video>
+
+      <div class="select-value" data-select-value-video>
+        Select category
+      </div>
+
+      <div class="select-icon">
+        <ion-icon name="chevron-down"></ion-icon>
+      </div>
+
+    </button>
+
+    <ul class="select-list" data-select-list>
+
+      <li class="select-item">
+
+        <button data-select-item-video>
+          All
+        </button>
+
+      </li>
+
+    </ul>
+
+  </div>
+
+  <ul class="project-list" data-canvas-list>
+
+  </ul>
+
+</section>
+
+
+<div class="modal-container" data-canvas-container>
+
+  <div class="overlay" data-overlay>
+  </div>
+
+  <section class="testimonials-modal">
+
+    <button class="modal-close-btn" data-canvas-close-btn>
+      <ion-icon name="close-outline"></ion-icon>
+    </button>
+
+    <div class="modal-img-wrapper">
+      <figure class="modal-avatar-box">
+      <!--<img src="./assets/images/avatar-1.png" alt="Daniel lewis" width="80" data-canvas-img>-->
+      </figure>
+
+      <img src="./assets/images/icon-quote.svg" alt="quote icon">
+    </div>
+
+    <div class="modal-content">
+
+      <h4 class="h3 modal-title" data-canvas-title></h4>
+
+      <time datetime="2021-06-14" data-canvas-time></time>
+      <div data-modal-text>
+        <img src="#" width="100%" data-canvas-img>
+        <a href="#" target="_blank" data-canvas-url>
+          <button class="credential-btn">
+            Show credentials<ion-icon name="exit-outline"></ion-icon>
+          </button>
+        </a>
+      <!--<iframe  width="80%" allowfullscreen="true" allowscriptaccess="always" frameborder="0" src="https://www.youtube.com/embed/Eaj7pNeIQhs" data-canvas-video>
+        </iframe>-->
+      </div>
+
+    </div>
+
+  </section>
+
+</div>
+`;
+
+const certificates_canvasList = certificates_section.querySelector("[data-canvas-list]");
+const certificates_filterList = certificates_section.querySelector("[data-filter-list]");
+const certificates_selectList = certificates_section.querySelector("[data-select-list]");
+
+certificates_items = [
+  {"url":"https://www.credly.com/badges/b4c56cfc-2293-4d97-a09f-cd73a3dc7b0e/linked_in_profile",
+  "thumbnail":"https://media.licdn.com/dms/image/sync/D4D10AQGkkIp0RZljjQ/image-shrink_800/0/1693900616484?e=1695466800&v=beta&t=JILo1secDw-GQF-4rb4epIZDqwdm6i-YYJv4o6FvQAc",
+  "title":"AWS Certified Cloud Practitioner (CLF)",
+  "category":"Amazon Web Services (AWS)",
+  "time":"Issued Jun 2023 · Expires Jun 2026",
+  "background":"white"},
+  {"url":"https://www.credly.com/badges/f5bc36cd-5f7e-4461-8db9-ab8dca322fe1/linked_in_profile",
+  "thumbnail":"https://media.licdn.com/dms/image/sync/D4D10AQFtr8wjNpK-SQ/image-shrink_800/0/1693900038057?e=1695466800&v=beta&t=pQ6I2Uxk2D6yKof0GlSrSRPTJvWScBCcJxXMByOGasU",
+  "title":"AWS Certified Machine Learning - Specialty (MLS)",
+  "category":"Amazon Web Services (AWS)",
+  "time":"Issued Aug 2023 · Expires Aug 2026",
+  "background":"white"},
+  {"url":"https://graduation.udacity.com/confirm/TGPZQQ7H",
+  "thumbnail":"assets/images/9ec34961-e7c0-4d8e-affa-c9c60e3c0457.jpg",
+  "title":"AWS Machine Learning Foundations",
+  "category":"Udacity",
+  "time":"Issued Oct 2021",
+  "background":"white"},
+  {"url":"https://graduation.udacity.com/confirm/LAP4JCEX",
+  "thumbnail":"assets/images/e7ee9d0d-db95-4ba7-a1f0-d50be0904c5d.jpg",
+  "title":"Advanced Data Analysis Nanodegree",
+  "category":"Udacity",
+  "time":"Issued Jul 2021",
+  "background":"white"},
+  {"url":"https://graduation.udacity.com/confirm/QJRXEFH7",
+  "thumbnail":"assets/images/1f53571f-d9e4-44df-a6c5-3853c38c343f.jpg",
+  "title":"Machine Learning Engineer Nanodegree",
+  "category":"Udacity",
+  "time":"Issued Jul 2021",
+  "background":"white"},
+];
+  
+
+  
+
+
+
+var certificates_categories = _.keys(_.countBy(certificates_items, function(certificates_item) { return certificates_item.category; }))
+
+
+function certificates_textFunc(value){
+  // console.log(value)
+  text =  `<li class="project-item  active" data-filter-item-video data-category="${value.category}">
+        <a href="#" data-videos-item data-url="${value.url}" data-time="${value.time}">
+        
+          <figure class="project-img">
+            <div class="project-item-icon-box">
+              <ion-icon name="eye-outline"></ion-icon>
+            </div>
+        
+            <img src="${value.thumbnail}" alt="${value.category}" loading="lazy" data-videos-avatar style="background-color: ${value.background};">
+          </figure>
+        
+          <h3 class="project-title" data-videos-title>${value.title}</h3>
+        
+          <p class="project-category">${value.category}</p>
+        
+        </a>
+  </li>
+  `;
+  certificates_canvasList.insertAdjacentHTML('beforeend', text);
+}
+
+certificates_items.forEach(certificates_textFunc);
+
+
+
+function certificates_catFunc(value){
+  text = 
+  `
+  <li class="filter-item">
+
+    <button data-filter-btn-video>
+      ${value}
+    </button>
+
+  </li>
+  `;
+  certificates_filterList.insertAdjacentHTML('beforeend', text);
+  text = 
+  `
+  <li class="select-item">
+
+    <button data-select-item-video>
+      ${value}
+    </button>
+
+  </li>
+  `;
+  certificates_selectList.insertAdjacentHTML('beforeend', text);
+}
+
+certificates_categories.forEach(certificates_catFunc);
+
+
+
+// testimonials variables
+const certificates_videosItem = certificates_section.querySelectorAll("[data-videos-item]");
+
+
+// modal variable
+const certificates_canvasContainer = certificates_section.querySelector("[data-canvas-container]");
+const certificates_canvasImg = certificates_section.querySelector("[data-canvas-img]");
+const certificates_canvasTitle = certificates_section.querySelector("[data-canvas-title]");
+const certificates_canvasTime = certificates_section.querySelector("[data-canvas-time]");
+const certificates_canvasURL = certificates_section.querySelector("[data-canvas-url]");
+const certificates_canvasCloseBtn = certificates_section.querySelector("[data-canvas-close-btn]");
+const certificates_overlay = certificates_section.querySelector("[data-overlay]");
+
+
+// canvas toggle function
+function certificates_canvasToggleFunc() {
+
+  certificates_canvasContainer.classList.toggle("active");
+  certificates_overlay.classList.toggle("active");
+}
+
+
+function certificates_canvasOpen() {
+
+    certificates_canvasImg.src = this.querySelector("[data-videos-avatar]").src;
+    certificates_canvasImg.alt = this.querySelector("[data-videos-avatar]").alt;
+    certificates_canvasTitle.innerHTML = this.querySelector("[data-videos-title]").innerHTML;
+    certificates_canvasTime.innerHTML = this.dataset.time;
+    certificates_canvasURL.href = this.dataset.url;
+    certificates_canvasToggleFunc();
+  }
+
+// add click event to all modal items
+for (let i = 0; i < certificates_videosItem.length; i++) {
+  certificates_videosItem[i].addEventListener("click",certificates_canvasOpen);
+}
+
+// add click event to modal close button
+certificates_canvasCloseBtn.addEventListener("click", certificates_canvasToggleFunc);
+certificates_overlay.addEventListener("click", certificates_canvasToggleFunc);
+
+
+//##############################################
+
+
+// custom select variables
+const certificates_selectVideo = certificates_section.querySelector("[data-select-video]");
+const certificates_selectItemsVideo = certificates_section.querySelectorAll("[data-select-item-video]");
+const certificates_selectValueVideo = certificates_section.querySelector("[data-select-value-video]");
+const certificates_filterBtnVideo = certificates_section.querySelectorAll("[data-filter-btn-video]");
+const certificates_filterItemsVideo = certificates_section.querySelectorAll("[data-filter-item-video]");
+let certificates_lastClickedBtnVideo = certificates_filterBtnVideo[0];
+
+
+// element toggle function
+function certificates_elementToggleVideoFunc(elem) 
+{ 
+    elem.classList.toggle("active"); 
+}
+
+function certificates_filterFuncVideo(selectedValue) 
+{
+
+    for (let i = 0; i < certificates_filterItemsVideo.length; i++) 
+    {
+        if (selectedValue === "All") 
+        {
+            certificates_filterItemsVideo[i].classList.add("active");
+        } 
+
+        else if (selectedValue === certificates_filterItemsVideo[i].dataset.category) 
+        {
+            certificates_filterItemsVideo[i].classList.add("active");
+        }
+        else 
+        {
+            certificates_filterItemsVideo[i].classList.remove("active");
+        }
+    }
+  
+}
+
+function certificates_filterFunc2Video() 
+{
+
+    let selectedValue = this.innerText; //.toLowerCase();
+    certificates_selectValueVideo.innerText = this.innerText;
+    certificates_elementToggleVideoFunc(certificates_selectVideo);
+    certificates_filterFuncVideo(selectedValue);
+
+}
+
+function certificates_largeScreenVideo() 
+{
+
+    let selectedValue = this.innerText; //.toLowerCase();
+    certificates_selectValueVideo.innerText = this.innerText;
+    certificates_filterFuncVideo(selectedValue);
+    certificates_lastClickedBtnVideo.classList.remove("active");
+    this.classList.add("active");
+    certificates_lastClickedBtnVideo = this;
+}
+
+
+certificates_selectVideo.addEventListener("click", function () { certificates_elementToggleVideoFunc(this); });
+
+// add event in all select items
+for (let i = 0; i < certificates_selectItemsVideo.length; i++) {
+  certificates_selectItemsVideo[i].addEventListener("click", certificates_filterFunc2Video);
+}
+
+// add event in all filter button items for large screen
+for (let i = 0; i < certificates_filterBtnVideo.length; i++) {
+  certificates_filterBtnVideo[i].addEventListener("click",certificates_largeScreenVideo);
+}
+
+
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,69 +1,285 @@
+const portfolio_section = document.querySelector(".portfolio");
+
+portfolio_section.innerHTML =
+`
+<header>
+<h2 class="h2 article-title">Portfolio</h2>
+</header>
+
+<section class="projects">
+
+  <ul class="filter-list" data-filter-list>
+
+    <li class="filter-item">
+
+      <button class="active" data-filter-btn-video>
+        All
+      </button>
+
+    </li>
+
+  </ul>
+
+  <div class="filter-select-box">
+
+    <button class="filter-select" data-select-video>
+
+      <div class="select-value" data-select-value-video>
+        Select category
+      </div>
+
+      <div class="select-icon">
+        <ion-icon name="chevron-down"></ion-icon>
+      </div>
+
+    </button>
+
+    <ul class="select-list" data-select-list>
+
+      <li class="select-item">
+
+        <button data-select-item-video>
+          All
+        </button>
+
+      </li>
+
+    </ul>
+
+  </div>
+
+  <ul class="project-list" data-canvas-list>
+
+  </ul>
+
+</section>
+
+
+<div class="modal-container" data-canvas-container>
+
+<div class="overlay" data-overlay></div>
+
+<section class="testimonials-modal">
+
+  <button class="modal-close-btn" data-canvas-close-btn>
+    <ion-icon name="close-outline"></ion-icon>
+  </button>
+
+  <div class="modal-img-wrapper">
+    <figure class="modal-avatar-box">
+      <img src="./assets/images/avatar-1.png" alt="Daniel lewis" width="80" data-canvas-img>
+    </figure>
+
+    <img src="./assets/images/icon-quote.svg" alt="quote icon">
+  </div>
+
+  <div class="modal-content">
+
+    <h4 class="h3 modal-title" data-canvas-title>Daniel lewis</h4>
+
+    <time datetime="2021-06-14">14 June, 2021</time>
+    <div data-modal-text>
+      <iframe  width="80%" allowfullscreen="true" allowscriptaccess="always" frameborder="0"
+              src="https://www.youtube.com/embed/Eaj7pNeIQhs" data-canvas-video>
+      </iframe>
+    </div>
+
+  </div>
+
+</section>
+
+</div>
+`
+
+const portfolio_canvasList = portfolio_section.querySelector("[data-canvas-list]");
+const portfolio_filterList = portfolio_section.querySelector("[data-filter-list]");
+const portfolio_selectList = portfolio_section.querySelector("[data-select-list]");
+
+portfolio_items = [
+  {"id":"BAx0LI6NnNg","title":"الهاكر","category":"الدحيح"},
+  {"id":"efigiluZEHA","title":"انسان حساس","category":"الدحيح"},
+  {"id":"tXZB5AJmrvM","title":"عيال اليومين دول","category":"الدحيح"},
+  {"id":"YPOiNF0riyA","title":"اخر يوم في العالم","category":"الدحيح"},
+  {"id":"4lEDoOcgjPQ","title":"حماقات الاذكياء","category":"الدحيح"},
+  {"id":"jRnBwscF9OU","title":"هل نلت اليوم قسطًا كافيًا من النوم؟","category":"بوكافيين"},
+  {"id":"OZhcNgsTbi8","title":"عندما تستخدمك الشركات الكبرى كسلعة","category":"بوكافيين"},
+  {"id":"owI9ung-T3w","title":"عندما اجتمع العرب في دولة واحدة","category":"بوكافيين"},
+  ];
+
+
+
+
+
+var portfolio_categories = _.keys(_.countBy(portfolio_items, function(portfolio_item) { return portfolio_item.category; }))
+
+
+function portfolio_textFunc(value){
+  // console.log(value)
+  text =  `<li class="project-item  active" data-filter-item-video data-category=${value.category}>
+        <a href="#" data-videos-item data-video="https://www.youtube.com/embed/${value.id}">
+        
+          <figure class="project-img">
+            <div class="project-item-icon-box">
+              <ion-icon name="eye-outline"></ion-icon>
+            </div>
+        
+            <img src="https://i.ytimg.com/vi/${value.id}/maxresdefault.jpg" alt=${value.category} loading="lazy" data-videos-avatar>
+          </figure>
+        
+          <h3 class="project-title" data-videos-title>${value.title}</h3>
+        
+          <p class="project-category">${value.category}</p>
+        
+        </a>
+  </li>
+  `;
+  portfolio_canvasList.insertAdjacentHTML('beforeend', text);
+}
+
+portfolio_items.forEach(portfolio_textFunc);
+
+
+
+function portfolio_catFunc(value){
+  text = 
+  `
+  <li class="filter-item">
+
+    <button data-filter-btn-video>
+      ${value}
+    </button>
+
+  </li>
+  `;
+  portfolio_filterList.insertAdjacentHTML('beforeend', text);
+  text = 
+  `
+  <li class="select-item">
+
+    <button data-select-item-video>
+      ${value}
+    </button>
+
+  </li>
+  `;
+  portfolio_selectList.insertAdjacentHTML('beforeend', text);
+}
+
+portfolio_categories.forEach(portfolio_catFunc);
+
+
+
+// testimonials variables
+const portfolio_videosItem = portfolio_section.querySelectorAll("[data-videos-item]");
+
+
+// modal variable
+const portfolio_canvasContainer = portfolio_section.querySelector("[data-canvas-container]");
+const portfolio_canvasImg = portfolio_section.querySelector("[data-canvas-img]");
+const portfolio_canvasTitle = portfolio_section.querySelector("[data-canvas-title]");
+const portfolio_canvasVideo = portfolio_section.querySelector("[data-canvas-video]");
+const portfolio_canvasCloseBtn = portfolio_section.querySelector("[data-canvas-close-btn]");
+const portfolio_overlay = portfolio_section.querySelector("[data-overlay]");
+
+
+// canvas toggle function
+function portfolio_canvasToggleFunc() {
+
+  portfolio_canvasContainer.classList.toggle("active");
+  portfolio_overlay.classList.toggle("active");
+  // reset source to stop the current video
+  portfolio_canvasVideo.src = portfolio_canvasVideo.src;
+}
+
+
+function portfolio_canvasOpen() {
+
+    portfolio_canvasImg.src = this.querySelector("[data-videos-avatar]").src;
+    portfolio_canvasImg.alt = this.querySelector("[data-videos-avatar]").alt;
+    portfolio_canvasTitle.innerHTML = this.querySelector("[data-videos-title]").innerHTML;
+    portfolio_canvasVideo.src = this.dataset.video;
+    portfolio_canvasToggleFunc();
+  }
+
+// add click event to all modal items
+for (let i = 0; i < portfolio_videosItem.length; i++) {
+  portfolio_videosItem[i].addEventListener("click",portfolio_canvasOpen);
+}
+
+// add click event to modal close button
+portfolio_canvasCloseBtn.addEventListener("click", portfolio_canvasToggleFunc);
+portfolio_overlay.addEventListener("click", portfolio_canvasToggleFunc);
+
+
+//##############################################
+
+
 // custom select variables
-const select = document.querySelector("[data-select]");
-const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
-const filterItems = document.querySelectorAll("[data-filter-item]");
-let lastClickedBtn = filterBtn[0];
+const portfolio_selectVideo = portfolio_section.querySelector("[data-select-video]");
+const portfolio_selectItemsVideo = portfolio_section.querySelectorAll("[data-select-item-video]");
+const portfolio_selectValueVideo = portfolio_section.querySelector("[data-select-value-video]");
+const portfolio_filterBtnVideo = portfolio_section.querySelectorAll("[data-filter-btn-video]");
+const portfolio_filterItemsVideo = portfolio_section.querySelectorAll("[data-filter-item-video]");
+let portfolio_lastClickedBtnVideo = portfolio_filterBtnVideo[0];
 
 
 // element toggle function
-function elementToggleFunc(elem) 
+function portfolio_elementToggleVideoFunc(elem) 
 { 
     elem.classList.toggle("active"); 
 }
 
-function filterFunc(selectedValue) 
+function portfolio_filterFuncVideo(selectedValue) 
 {
 
-    for (let i = 0; i < filterItems.length; i++) 
+    for (let i = 0; i < portfolio_filterItemsVideo.length; i++) 
     {
         if (selectedValue === "all") 
         {
-            filterItems[i].classList.add("active");
+            portfolio_filterItemsVideo[i].classList.add("active");
         } 
-        else if (selectedValue === filterItems[i].dataset.category) 
+        else if (selectedValue === portfolio_filterItemsVideo[i].dataset.category) 
         {
-            filterItems[i].classList.add("active");
+            portfolio_filterItemsVideo[i].classList.add("active");
         }
         else 
         {
-            filterItems[i].classList.remove("active");
+            portfolio_filterItemsVideo[i].classList.remove("active");
         }
     }
   
 }
 
-function filterFunc2() 
+function portfolio_filterFunc2Video() 
 {
 
     let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
+    portfolio_selectValueVideo.innerText = this.innerText;
+    portfolio_elementToggleVideoFunc(portfolio_selectVideo);
+    portfolio_filterFuncVideo(selectedValue);
 
 }
 
-function largeScreen() 
+function portfolio_largeScreenVideo() 
 {
 
     let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-    lastClickedBtn.classList.remove("active");
+    portfolio_selectValueVideo.innerText = this.innerText;
+    portfolio_filterFuncVideo(selectedValue);
+    portfolio_lastClickedBtnVideo.classList.remove("active");
     this.classList.add("active");
-    lastClickedBtn = this;
+    portfolio_lastClickedBtnVideo = this;
 }
 
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+portfolio_selectVideo.addEventListener("click", function () { portfolio_elementToggleVideoFunc(this); });
 
 // add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", filterFunc2);
+for (let i = 0; i < portfolio_selectItemsVideo.length; i++) {
+  portfolio_selectItemsVideo[i].addEventListener("click", portfolio_filterFunc2Video);
 }
 
 // add event in all filter button items for large screen
-for (let i = 0; i < filterBtn.length; i++) {
-  filterBtn[i].addEventListener("click",largeScreen);
+for (let i = 0; i < portfolio_filterBtnVideo.length; i++) {
+  portfolio_filterBtnVideo[i].addEventListener("click",portfolio_largeScreenVideo);
 }
